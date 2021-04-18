36b0c042d02577e6f6e95e8967f6a689e01b8cb2
Alexander-Aparicio <73755700+Alexander-Aparicio@users.noreply.github.com>
2021-04-18 15:27:24 -0500

    implementando código a juego

--- docs/revolution-js/juegos.js
+++ docs/revolution-js/juegos.js
@@ -4,9 +4,16 @@ export function masPalabrasPG (){
 
     let $btnJuego = d.querySelector('#juego1'),
         $formJuego1 = d.querySelector('.modal-juego1'),
-        $closeform1 = d.querySelector('.close-juego1')
+        $closeform1 = d.querySelector('.close-juego1'),
+        $btnPlay = d.querySelector('.btn-juego1'),
+        $descriptionGame = d.querySelector('.head-games'),
+        $formPlayOne = d.getElementById('form-play1'),
+        $headerPlay = d.querySelector('.juego'),
+        $listOption = d.getElementById('list'),
+        $cortina = d.querySelector('.background-dark')
+    
     d.addEventListener('click', (e)=>{
-
+        e.preventDefault()
         if(e.target.matches('#juego1')){
             $formJuego1.classList.toggle('none')
         }
@@ -15,7 +22,22 @@ export function masPalabrasPG (){
             $formJuego1.classList.toggle('none')
         }
 
+        if(e.target.matches('.btn-juego1')){
+            $descriptionGame.classList.add('none')
+            let playerOne = d.getElementById('jugador1').value
+            let playerTwo = d.getElementById('jugador2').value
+            let playerThree = d.getElementById('jugador3').value
+            let playerFour = d.getElementById('jugador4').value
+            
+            $listOption.insertAdjacentHTML("beforebegin",`<p id="turn"></p>`)
+            
+            d.getElementById('turn').textContent = `Es tu turno ${playerOne}`
+            $formPlayOne.classList.add('animation-close-der-izq')
+            $formPlayOne.style.setProperty('transform','translateX(-120%)')
+        } 
     })
 
-
+    let options = ['#op1','#op2','#op3','#op4','#op5','#op6','#op7','#op8','#op9','#op10','#op11','#op12']
+    // let options = ['#op1','#op2','#op3','#op4','#op5','#op6','#op7','#op8','#op9','#op10','#op11','#op12']
+    
 }
